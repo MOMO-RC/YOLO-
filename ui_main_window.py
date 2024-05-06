240af29494fff1deb868d182a9c29c2e253f548b
Coffee <83170484+MOMO-RC@users.noreply.github.com>
2024-05-06 18:54:39 +0800

    Add files via upload
    新加入了pyside6界面作为演示。现在有3种方法检测：
    一：直接输入代码：python detect.py --weigths best.pt --source 视频路径
    二：Gradio界面：打开Gradio_demo.py文件，点击播放，出现Gradio界面，进行图片检测
    三：Pyside6界面：打开base_ui.py，点击运行，出现本人设计的Pyside界面，可进行图片检测和视频检测。

---
+++ ui_main_window.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 336)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.input = QLabel(self.centralwidget)
+        self.input.setObjectName(u"input")
+        self.input.setGeometry(QRect(40, 20, 321, 201))
+        self.input.setScaledContents(True)
+        self.input.setAlignment(Qt.AlignCenter)
+        self.output = QLabel(self.centralwidget)
+        self.output.setObjectName(u"output")
+        self.output.setGeometry(QRect(420, 20, 321, 201))
+        self.output.setScaledContents(True)
+        self.output.setAlignment(Qt.AlignCenter)
+        self.line = QFrame(self.centralwidget)
+        self.line.setObjectName(u"line")
+        self.line.setGeometry(QRect(380, 0, 20, 221))
+        self.line.setFrameShape(QFrame.VLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+        self.det_image = QPushButton(self.centralwidget)
+        self.det_image.setObjectName(u"det_image")
+        self.det_image.setGeometry(QRect(40, 270, 321, 24))
+        self.det_video = QPushButton(self.centralwidget)
+        self.det_video.setObjectName(u"det_video")
+        self.det_video.setGeometry(QRect(420, 270, 321, 24))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.input.setText(QCoreApplication.translate("MainWindow", u"\u539f\u59cb\u56fe\u50cf", None))
+        self.output.setText(QCoreApplication.translate("MainWindow", u"\u68c0\u6d4b\u7ed3\u679c", None))
+        self.det_image.setText(QCoreApplication.translate("MainWindow", u"\u56fe\u7247\u68c0\u6d4b", None))
+        self.det_video.setText(QCoreApplication.translate("MainWindow", u"\u89c6\u9891\u68c0\u6d4b", None))
+    # retranslateUi
+
